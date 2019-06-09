5c39a56980959d5bdff1a9590b321fd60b13f32f
ChoiYura <s2017i19@e-mirim.hs.kr>
2019-06-09 17:02:50 +0900

    아이템 블럭 추가

--- js/stage1.js
+++ js/stage1.js
@@ -1,3 +1,9 @@
+window.onload = init;
+
+var canvas;
+var ctx;
+var img;
+
 var num = 1;
 
 var walk_v = 0; // 걷기 값
@@ -6,8 +12,20 @@ var rotate_v = 0; // 회전 값
 var now_location = 0; // 현재 위치
 var now_rotation = 0; // 현재 회전
 
+var itemFlag = 0; //아이템 사용 유무(아이템 여러게 되면 배열로 쓰면 될 듯)
+
 var distinction = [0,]; // 구별할 수 있는 배열
 
+function init() {
+    canvas = document.getElementById("gameMain");
+    ctx = canvas.getContext("2d");
+    img = new Image();
+    img.src = "pic/stage/obstacle/water.png";
+
+    img.onload = function(e) {
+        ctx.drawImage(img, 200, 300, 50, 70);
+    }
+}
 
 // '걷기' 블록 -> 노란색
 function walk(){
@@ -38,15 +56,24 @@ function mutiply(){
 function rotation(){
 
     distinction[num] = 'rotate'; 
-    rotate_v = 90; // 90도 돌기 (기본값)
+    rotate_v += 90; // 90도 돌기 (기본값)
 
     document.getElementById("play"+num).style.backgroundColor = "blue";
     num++;
 }
 
+// '아이템' 블록 -> 보라색
+function item() {
+
+    distinction[num] = 'item';
+    itemFlag = 1;
+
+    document.getElementById("play"+num).style.backgroundColor = "purple";
+    num++;
+}
+
 // '쓰레기' 블록 -> 흰색
 function trash(){
-
 
     num--;
 
@@ -63,12 +90,25 @@ function trash(){
 
 // 플레이 버튼
 function play(){
-
     let cat = document.getElementById("cat");
 
     // 누적하기
     now_location += walk_v;
     now_rotation += rotate_v;
+
+    // 위치 검사(장애물 위치와 겹치면 게임 오버)
+    if(now_location+100 > 200 && itemFlag == 0) {
+        alert("게임 오버되었습니다.");
+        now_location -= walk_v;
+        now_rotation -= rotate_v;
+        location.reload();
+    } else if(itemFlag == 1) {
+        img.src = "pic/stage/item/towel.jpg";
+        img.onload = function(e) {
+            ctx.drawImage(img, 200, 300, 50, 70);
+        }
+        itemFlag = 2;
+    }
 
     // 확인용
     console.log(now_location+"만큼 걷기");
@@ -78,4 +118,10 @@ function play(){
     cat.style.left = now_location + "px";
     cat.style.transform = 'rotate('+now_rotation+'deg)';
     
+    walk_v -= walk_v;
+    rotate_v -= rotate_v;
+
+    for(var i=num; i>0; i--) {
+        trash();
+    }
 }
